afa52c3789e4ae56f6370fbae98790eddc1896b4
huytocdo <ndhuyspkt@gmail.com>
2018-09-08 14:13:58 +0700

    complete project

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,59 @@
 defmodule Cards do
+
   @moduledoc """
-  Documentation for Cards.
+    Provides methods for creating and handling a deck of cards
   """
 
   @doc """
-  Hello world.
+    Return a list of strings representing a deck of playing cards
+  """
+  def create_deck do
+    values = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
+    suits = ["Spades","Clubs","Hearts","Diamond"]
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @doc """
+    Determines whether a deck contains a given card
 
   ## Examples
-
-      iex> Cards.hello()
-      :world
-
+  
+      iex> deck = Cards.create_deck
+      iex> Cards.contain?(deck, "Ace of Spades")
+      true    
+      
   """
-  def hello do
-    :world
+  def contain?(deck, card) do
+    Enum.member?(deck,card)
+  end  
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _} -> "Can't not find!"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size) 
+  end
+
 end
